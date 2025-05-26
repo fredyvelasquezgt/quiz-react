baa2a110142e969b471414b86e0809c772af394b
Fredy Velasquez <fredy.velasquez@telus.com>
2025-05-26 17:14:45 -0600

    context and provider created

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,26 +1,18 @@
 import Question from "./Question";
-import {useReducer} from 'react';
+import {useContext} from 'react';
+import { QuizContext } from "../contexts/quiz";
 
-const initialState = {
-    currentQuestionIndex: 0,
-    questions: [],
-};
 
-const reducer = (state, action) => {
-    if(action.type === 'NEXT_QUESTION') {
-        return {...state, currentQuestionIndex: state.currentQuestionIndex+1};
-    }
-    return state;
-};
 
 const Quiz = () => {
-    const [state, dispatch] = useReducer(reducer, initialState)
+    const [quizState, dispatch] = useContext(QuizContext)
+    console.log(quizState);
  
     return (
     <div className="quiz">
         <div>
             <div className="score">Question 1/8</div> 
-            <Question />
+            <Question  />
             <div className="next-button" onClick={()=>dispatch({type: 'NEXT_QUESTION'})}>Next Question </div>
         </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Quiz from './components/Quiz';
+import { QuizProvider } from './contexts/quiz';
 import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Quiz />
+    <QuizProvider>
+        <Quiz />
+    </QuizProvider>
  );
 
